86a7c407eeec6845b123aff588d32229e2e43b6d
Greg Morgan <gregm9999@gmail.com>
2018-11-09 20:19:33 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    counter=0
+    sentAr=self.split(" ")
+    sentAr.each do |sent|
+      if sent.sentence?
+        counter+=1
+      elsif sent.question?
+        counter+=1
+      elsif sent.exclamation?
+        counter+=1
+      end
+    end
+    counter
   end
 end
