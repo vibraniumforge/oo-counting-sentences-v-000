40a37023cbad6d08ad20cff098f30ed79fb1106e
Greg Morgan <gregm9999@gmail.com>
2018-11-09 20:13:21 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,14 @@ class String
   end
 
   def count_sentences
-
+    counter=0
+    if self.sentence?
+      counter+=1
+    elsif self.question?
+      counter+=1
+    elsif self.exclamation?
+      counter+=1
+    end
+    counter
   end
 end
